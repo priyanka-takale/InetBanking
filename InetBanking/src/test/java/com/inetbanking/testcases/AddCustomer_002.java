3a1e483105e8bf43c171e540502166bb8edc5632
priya <priyankatakale11@gmail.com>
2022-02-24 16:48:36 +0530

    Priyanka : Update Naw test case

--- InetBanking/src/test/java/com/inetbanking/testcases/AddCustomer_002.java
+++ InetBanking/src/test/java/com/inetbanking/testcases/AddCustomer_002.java
@@ -4,7 +4,8 @@ public class AddCustomer_002 {
 
 	public static void main(String[] args) {
 	
-             System.out.println("This is new test case");
+             System.out.println("This is new test case in this java class");
+             
 	}
 
 }
