ebc290c98b7efaa71ce955a705e5b241bb9d5581
priya <priyankatakale11@gmail.com>
2022-02-24 17:03:32 +0530

    Priyanka: Updated Now

--- InetBanking/src/test/java/com/inetbanking/testcases/DeleteCustomer.java
+++ InetBanking/src/test/java/com/inetbanking/testcases/DeleteCustomer.java
@@ -1,2 +1,3 @@
 // This is delete customer code java file code yet to commit
 // This is delete customer code java file code yet to commit
+// This is delete customer code java file code yet to commit
